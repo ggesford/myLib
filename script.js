b6ba14956b384ecfd759b4a0be60e1553de8ec15
ggesford <gageg0411@gmail.com>
2025-09-22 19:19:59 -0700

    fix: refactor for organization of seperate concerns such as UI and data

--- script.js
+++ script.js
@@ -1,37 +1,53 @@
+//UI Rendering//
+
 const addDialog = document.querySelector(".add-modal");
 const rateDialog = document.querySelector(".rate-modal");
 const openAddModal = document.querySelector(".header__add-btn");
 const readStatusRadioSet = document.getElementsByName("location");
 const parentDiv = document.querySelector(".u-form-wrapper");
 const rateReviewForm = createRateFormSection();
-const bookGridWrapper = querySelector(".shelf__cards");
+const bookGridWrapper = document.querySelector(".shelf__cards");
 
-openAddModal.addEventListener("click", () => addDialog.showModal());
-
-addDialog.addEventListener("submit", (event) => {
-  event.preventDefault();
-  const formData = new FormData(event.target);
-  const title = formData.get("title");
-  const author = formData.get("author");
-  const pageCount = formData.get("pageCount");
-  const location = formData.get("location");
-  const rating = formData.get("rating");
-  const review = formData.get("review");
-
-  const newBook = new Book(title, author, pageCount, location, rating, review);
-  library.appendBook(newBook);
-});
-
-readStatusRadioSet.forEach((radio) =>
-  radio.addEventListener("change", (event) => {
-    const chosen = event.target;
-    if (chosen.value === "completeBook") {
-      appendRateFormSection(createRateFormSection());
-    } else {
-      deleteRateFormSection();
+function renderBook(book) {
+  const bookCard = document.createElement("div");
+  bookCard.dataset.id = book.id;
+  const bookTitleAuthor = document.createElement("h3");
+  bookTitleAuthor.textContent = `${book.title}, ${book.author}`;
+  bookCard.appendChild(bookTitleAuthor);
+  const bookPageCount = document.createElement("p");
+  bookPageCount.textContent = `Page Count: ${book.pageCount}`;
+  bookCard.appendChild(bookPageCount);
+  if (book.location === "completeBook") {
+    const bookRating = document.createElement("div");
+    const bookRatingTitle = document.createElement("p");
+    bookRatingTitle.textContent = "Rating:";
+    bookRating.appendChild(bookRatingTitle);
+    const bookRatingIcons = document.createElement("div");
+    for (let i = 1; i <= (book.rate || 0); i++) {
+      const bookRatingIcon = document.createElement("img");
+      bookRatingIcon.src =
+        "assets/images/bookmark-favorite-rating-star-svgrepo-com.svg";
+      bookRatingIcons.appendChild(bookRatingIcon);
     }
-  })
-);
+    bookRating.appendChild(bookRatingIcons);
+    bookCard.appendChild(bookRating);
+  }
+  const bookEdit = document.createElement("button");
+  const bookEditIcon = document.createElement("img");
+  bookEditIcon.src = "assets/images/gear-svgrepo-com.svg";
+  bookEdit.appendChild(bookEditIcon);
+  bookCard.appendChild(bookEdit);
+  const bookDelete = document.createElement("button");
+  const bookDeleteIcon = document.createElement("img");
+  bookDeleteIcon.src = "assets/images/trash-blank-alt-svgrepo-com.svg";
+  bookDelete.appendChild(bookDeleteIcon);
+  bookCard.appendChild(bookDelete);
+  bookGridWrapper.appendChild(bookCard);
+}
+
+function renderBooks(shelf) {
+  shelf.filterBooks().forEach(renderBook);
+}
 
 function createRateFormSection() {
   const rateReviewSection = document.createElement("fieldset");
@@ -68,13 +84,17 @@ function createRateFormSection() {
   rateReviewSection.appendChild(reviewInput);
   return rateReviewSection;
 }
+
 function deleteRateFormSection() {
   parentDiv.removeChild(rateReviewForm);
 }
+
 function appendRateFormSection() {
   const belowButton = document.querySelector("#insertBefore");
   parentDiv.insertBefore(rateReviewForm, belowButton);
 }
+
+//Data Handling//
 
 const library = {
   bookList: [],
@@ -94,44 +114,6 @@ function Shelf(location) {
   this.filterBooks = function () {
     return library.bookList.filter((book) => book.location === this.location);
   };
-  this.renderBooks = function () {
-    this.filterBooks().forEach((book) => this.renderBook(book));
-  };
-  this.renderBook = function (book) {
-    const bookCard = document.createElement("div");
-    const bookTitleAuthor = document.createElement("h3");
-    bookTitleAuthor.textContent = `${book.title}, ${book.author}`;
-    bookCard.appendChild(bookTitleAuthor);
-    const bookPageCount = document.createElement("p");
-    bookPageCount.textContent = `Page Count: ${book.pageCount}`;
-    bookCard.appendChild(bookPageCount);
-    if (book.location === "completeBook") {
-      const bookRating = document.createElement("div");
-      const bookRatingTitle = document.createElement("p");
-      bookRatingTitle.textContent = "Rating:";
-      bookRating.appendChild(bookRatingTitle);
-      const bookRatingIcons = document.createElement("div");
-      for (let i = 1; i <= (book.rate || 0); i++) {
-        const bookRatingIcon = document.createElement("img");
-        bookRatingIcon.src =
-          "assets/images/bookmark-favorite-rating-star-svgrepo-com.svg";
-        bookRatingIcons.appendChild(bookRatingIcon);
-      }
-      bookRating.appendChild(bookRatingIcons);
-      bookCard.appendChild(bookRating);
-    }
-    const bookEdit = document.createElement("button");
-    const bookEditIcon = document.createElement("img");
-    bookEditIcon.src = "assets/images/gear-svgrepo-com.svg";
-    bookEdit.appendChild(bookEditIcon);
-    bookCard.appendChild(bookEdit);
-    const bookDelete = document.createElement("button");
-    const bookDeleteIcon = document.createElement("img");
-    bookDeleteIcon.src = "assets/images/trash-blank-alt-svgrepo-com.svg";
-    bookDelete.appendChild(bookDeleteIcon);
-    bookCard.appendChild(bookDelete);
-    bookGridWrapper.appendChild(bookCard);
-  };
 }
 
 function Book(title, author, pageCount, location, rate, review) {
@@ -143,3 +125,32 @@ function Book(title, author, pageCount, location, rate, review) {
   this.review = review;
   this.id = crypto.randomUUID();
 }
+
+//Event Listeners//
+
+openAddModal.addEventListener("click", () => addDialog.showModal());
+
+addDialog.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const formData = new FormData(event.target);
+  const title = formData.get("title");
+  const author = formData.get("author");
+  const pageCount = formData.get("pageCount");
+  const location = formData.get("location");
+  const rating = formData.get("rating");
+  const review = formData.get("review");
+
+  const newBook = new Book(title, author, pageCount, location, rating, review);
+  library.appendBook(newBook);
+});
+
+readStatusRadioSet.forEach((radio) =>
+  radio.addEventListener("change", (event) => {
+    const chosen = event.target;
+    if (chosen.value === "completeBook") {
+      appendRateFormSection(createRateFormSection());
+    } else {
+      deleteRateFormSection();
+    }
+  })
+);
